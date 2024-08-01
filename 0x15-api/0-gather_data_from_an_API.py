0a5a84133066bc18ed28c7bd5b94e9a0986e5d24
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-08-01 14:59:00 -0700

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-import re
+"""
+Returns to-do list information for a given employee ID.
+
+This script takes an employee ID as a command-line argument and fetches
+the corresponding user information and to-do list from the JSONPlaceholder API.
+It then prints the tasks completed by the employee.
+"""
+
 import sys
 import requests
 """Get employee data from API"""
 
 
 if __name__ == '__main__':
+    # Get the employee information using the provided employee ID
     user_id = sys.argv[1]
+    # Base URL for the JSONPlaceholder API
     url = 'https://jsonplaceholder.typicode.com/'
     response = requests.get(url + f'users/{user_id}').json()
+    # Get the to-do list for the employee using the provided employee ID
     payload = {'userId': user_id}
     todo_response = requests.get(url + 'todos', params=payload)
     todos = todo_response.json()
