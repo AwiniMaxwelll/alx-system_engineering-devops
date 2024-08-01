3428aee34e16fdcbb51e2eef9dac2af5e5660d16
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-08-01 13:56:48 -0700

    your Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,23 +7,24 @@ import sys
 
 
 if __name__ == "__main__":
-     # Get the user ID from the command-line arguments provided to the script
+    # Get the user ID from the command-line arguments provided to the script
     user_id = sys.argv[1]
 
-     # Base URL for the JSONPlaceholder API
+    # Base URL for the JSONPlaceholder API
     url = 'https://jsonplaceholder.typicode.com/'
 
     # Get the employee information using the provided employee ID
     user_response = requests.get(url+'users/{}'.format(user_id))
     user = user_response.json()
 
-     # Get the to-do list for the employee using the provided employee ID
+    # Get the to-do list for the employee using the provided employee ID
     payload = {'userId': user_id}
     todo_response = requests.get(url+'todos', params=payload)
     todos = todo_response.json()
 
     # Write each item's details (user ID, username, completion status,
-    #   and title) as a row in the CSV file
+    # and title) as a row in the CSV file
     with open(f'{user_id}.csv', 'w', newline='') as f_csv:
         writer = csv.writer(f_csv, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id,  user.get('username'), todo.get('completed'), todo.get('title')]) for todo in todos]
+        [writer.writerow([user_id,  user.get('username'), todo.get('completed'),
+        todo.get('title')]) for todo in todos]
