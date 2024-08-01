7027a831f28048585bb20b5c00c5b5e9b926bb12
Awinimaxwelll <awinimaxwell428@gmail.com>
2024-08-01 12:14:19 -0700

    Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
+
 import requests
 import csv
 import sys
-"""Get employee data from API"""
 
 
 if __name__ == "__main__":
+     # Get the user ID from the command-line arguments provided to the script
+    ser_id = sys.argv[1]
+
      # Base URL for the JSONPlaceholder API
     url = 'https://jsonplaceholder.typicode.com/'
+
     # Get the employee information using the provided employee ID
-    user_id = sys.argv[1]
     user_response = requests.get(url+'users/{}'.format(user_id))
     user = user_response.json()
+
      # Get the to-do list for the employee using the provided employee ID
     payload = {'userId': user_id}
     todo_response = requests.get(url+'todos', params=payload)
     todos = todo_response.json()
+
     # Write each item's details (user ID, username, completion status,
     #   and title) as a row in the CSV file
     with open(f'{user_id}.csv', 'w', newline='') as f_csv:
