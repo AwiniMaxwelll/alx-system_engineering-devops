1d286d29eef0be9b7d79e19c3903386577a51eb8
AwiniMaxwelll <awinimaxwell428@gmail.com>
2024-08-17 11:44:50 +0000

    funtion that returns the number of subscribers in a givensubreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,10 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/lordmax054)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    # return 0 if there is an error
     if response.status_code == 404:
         return 0
+    # return the number subscribers if successful
     results = response.json().get("data")
     result = results.get("subscribers")
     return result
